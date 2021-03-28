9051b3b63e58daeecebc7a6f64b15c2bd97102e1
Songguohao <947662512@qq.com>
2021-03-28 12:22:57 +0800

    获取Home页后端数据

---
+++ src/components/common/navbar/NavBar.vue
@@ -0,0 +1,38 @@
+<!--  -->
+<template>
+  <div class="nav-bar">
+    <div class="left"><slot name="left"></slot></div>
+    <div class="center"><slot name="center"></slot></div>
+    <div class="right"><slot name="right"></slot></div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "NavBar",
+  data() {
+    return {};
+  },
+  //生命周期 - 创建完成（访问当前this实例）
+  created() {},
+  //生命周期 - 挂载完成（访问DOM元素）
+  mounted() {},
+};
+</script>
+<style scoped>
+/* @import url(); 引入css类 */
+.nav-bar {
+  display: flex;
+  height: 44px;
+  line-height: 44px;
+  text-align: center;
+}
+
+.left,.right {
+  width: 60px;
+}
+
+.center {
+  flex: 1;
+}
+</style>
--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,20 +1,45 @@
 <!--  -->
 <template>
-  <h2>首页</h2>
+  <div id="home">
+    <nav-bar class="home-nav">
+      <div slot="center">购物车</div>
+    </nav-bar>
+  </div>
 </template>
 
 <script>
+import NavBar from "../../components/common/navbar/NavBar.vue";
+import { getHomeMultidata } from "network/home";
 export default {
+  components: { NavBar },
   name: "Home",
   data() {
-    return {};
+    return {
+      banners: [],
+      recommends: [],
+    };
   },
   //生命周期 - 创建完成（访问当前this实例）
-  created() {},
+  created() {
+    this.getHomeData();
+  },
   //生命周期 - 挂载完成（访问DOM元素）
   mounted() {},
+  methods: {
+    getHomeData() {
+      getHomeMultidata().then((res) => {
+        this.banners = res.data.data.banner.list;
+        this.recommends = res.data.data.recommend.list;
+      });
+    },
+  },
 };
 </script>
 <style scoped>
 /* @import url(); 引入css类 */
+@import "../../assets/css/basecss.css";
+.home-nav {
+  background-color: var(--color-tint);
+  color: #fff;
+}
 </style>
