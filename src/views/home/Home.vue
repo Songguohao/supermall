f716738e6458e21bfc8e227e590a30b9303a032b
Songguohao <947662512@qq.com>
2021-04-05 12:03:49 +0800

    主页修改

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -2,33 +2,92 @@
 <template>
   <div id="home">
     <nav-bar class="home-nav">
-      <div slot="center">购物车</div>
+      <div slot="center">购物街</div>
     </nav-bar>
     <home-swiper :banners="banners"></home-swiper>
     <recommend-view :recommends="recommends"></recommend-view>
     <feature-view></feature-view>
+    <tab-control
+      class="tab-control"
+      :titles="['流行', '新款', '精选']"
+    ></tab-control>
+    <div>列表1</div>
+    <div>列表1</div>
+    <div>列表1</div>
+    <div>列表1</div>
+    <div>列表1</div>
+    <div>列表1</div>
+    <div>列表1</div>
+    <div>列表1</div>
+    <div>列表1</div>
+    <div>列表1</div>
+    <div>列表1</div>
+    <div>列表1</div>
+    <div>列表1</div>
+    <div>列表1</div>
+    <div>列表1</div>
+    <div>列表1</div>
+    <div>列表1</div>
+    <div>列表1</div>
+    <div>列表1</div>
+    <div>列表1</div>
+    <div>列表1</div>
+    <div>列表1</div>
+    <div>列表1</div>
+    <div>列表1</div>
+    <div>列表1</div>
+    <div>列表1</div>
+    <div>列表1</div>
+    <div>列表1</div>
+    <div>列表1</div>
+    <div>列表1</div>
+    <div>列表1</div>
+    <div>列表1</div>
+    <div>列表1</div>
+    <div>列表1</div>
+    <div>列表1</div>
+    <div>列表1</div>
+    <div>列表1</div>
+    <div>列表1</div>
+    <div>列表1</div>
+    <div>列表1</div>
+    <div>列表1</div>
+    <div>列表1</div>
+    <div>列表1</div>
+    <div>列表1</div>
+    <div>列表1</div>
+    <div>列表1</div>
+    <div>列表1</div>
+    <div>列表1</div>
   </div>
 </template>
 
 <script>
 import NavBar from "../../components/common/navbar/NavBar.vue";
-import { getHomeMultidata } from "network/home";
+import { getHomeMultidata, getHomeGoods } from "network/home";
 import HomeSwiper from "./childComps/HomeSwiper.vue";
-import RecommendView from './childComps/RecommendView.vue';
-import FeatureView from './childComps/FeatureView.vue';
+import RecommendView from "./childComps/RecommendView.vue";
+import FeatureView from "./childComps/FeatureView.vue";
+import TabControl from "../../components/content/tabControl/TabControl.vue";
 
 export default {
-  components: { NavBar, HomeSwiper, RecommendView, FeatureView },
+  components: { NavBar, HomeSwiper, RecommendView, FeatureView, TabControl },
   name: "Home",
   data() {
     return {
       banners: [],
       recommends: [],
+      goods: {
+        'pop': { page: 0, list: [] },
+        'news': { page: 0, list: [] },
+        'sell': { page: 0, list: [] },
+      },
     };
   },
   //生命周期 - 创建完成（访问当前this实例）
   created() {
     this.getHomeData();
+    this.getHomeGoodsData('pop',1);
   },
   //生命周期 - 挂载完成（访问DOM元素）
   mounted() {},
@@ -39,6 +98,13 @@ export default {
         this.recommends = res.data.data.recommend.list;
       });
     },
+
+    //获取商品数据
+    getHomeGoodsData(type,page){
+      getHomeGoods(type,page).then(res => {
+        console.log(res)
+      })
+    }
   },
 };
 </script>
@@ -57,5 +123,10 @@ export default {
   right: 0;
   top: 0;
   z-index: 9;
+}
+
+.tab-control {
+  position: sticky;
+  top: 44px;
 }
 </style>
--- src/views/home/childComps/FeatureView.vue
+++ src/views/home/childComps/FeatureView.vue
@@ -17,6 +17,9 @@ export default {
 </script>
 
 <style scoped>
+  .feature{
+    display: flex
+  }
   .feature img{
     width: 100%;
   }
