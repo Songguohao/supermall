e8d1a547cb1527e5decafd247849bd525e07e0dd
Songguohao <947662512@qq.com>
2021-05-11 19:41:19 +0800

    详情轮播图

--- src/views/detail/Detail.vue
+++ src/views/detail/Detail.vue
@@ -2,22 +2,29 @@
 <template>
   <div id="detail">
     <detail-nav-bar></detail-nav-bar>
+    <detail-swiper :topImages="topImages"></detail-swiper>
   </div>
 </template>
 
 <script>
-import DetailNavBar from './childComps/DetailNavBar.vue';
+import DetailNavBar from "./childComps/DetailNavBar.vue";
+import { getDetail } from "network/detail";
+import DetailSwiper from './childComps/DetailSwiper.vue';
 export default {
-  components: { DetailNavBar },
+  components: { DetailNavBar, DetailSwiper },
   name: "Detail",
   data() {
     return {
       iid: null,
+      topImages: [],
     };
   },
   //生命周期 - 创建完成（访问当前this实例）
   created() {
     this.iid = this.$route.params.iid;
+    getDetail(this.iid).then((res) => {
+      this.topImages = res.data.result.itemInfo.topImages;
+    });
   },
   //生命周期 - 挂载完成（访问DOM元素）
   mounted() {},
